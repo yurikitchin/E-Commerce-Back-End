17115b109a7eec17721dedec4a8ce1008b656b03
Yuri Kitchin <ykitchin@gmail.com>
2021-07-17 14:24:27 +0800

    add update route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -49,8 +49,23 @@ router.post('/', async (req, res) => {
 
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const update = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where:{
+          id: req.params.id
+        },
+      });
+      console.log(req.params.id)
+      res.status(200).json(update)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.delete('/:id', (req, res) => {
