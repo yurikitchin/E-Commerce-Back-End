f45dc375a42a6adb3d30bd00df26ff25c04c848e
Yuri Kitchin <ykitchin@gmail.com>
2021-07-17 17:36:30 +0800

    add category delete route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -68,8 +68,18 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCat = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(deleteCat)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
